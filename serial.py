793cb686fa8f2df75ce8e52882999c463ccd3601
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-11 18:20:45 -0400

    Finish serial.py and got it reviewed

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,22 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Creates a serial generator from a starting number"""
+        self.start = start
+        self.steps = 0
+
+    def __repr__(self):
+        """Gives a string representation of the class instance"""
+        return f"<SerialGenerator start = {self.start} steps = {self.steps}>"
+
+    def generate(self):
+        """Returns the next number in the series starting at start num"""
+        return_num = self.start + self.steps
+        self.steps += 1
+        return return_num
+
+    def reset(self):
+        """Resets the steps of the SerialGenerator instance"""
+        self.steps = 0
